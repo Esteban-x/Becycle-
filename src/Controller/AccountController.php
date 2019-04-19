f1ef7002b39a9d8bf68ede6f40eef8fd561c0ce7
mymilouda <mymilouda59@gmail.com>
2019-04-19 12:04:17 +0200

    adresse

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Address;
+use App\Form\AddressType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AccountController extends AbstractController
@@ -14,6 +17,30 @@ class AccountController extends AbstractController
     {
         return $this->render('account/index.html.twig', [
             'controller_name' => 'AccountController',
+        ]);
+    }
+
+    /**
+     * @route("/adresse/new", name="adresse_new")
+     */
+    public function addAddress(Request $request, Address $address){
+
+        $form = $this->createForm(AddressType::class, $address);
+
+        $form->handleRequest($request);
+
+        dump($request);
+        if($form->isSubmitted() && $form->isValid())
+        {
+            dump($form->getData());
+
+            $this->addFlash("success", "Envoie bien prise en compte.");
+
+
+        }
+
+        return $this->render('account/address.html.twig', [
+            'form' => $form->createView()
         ]);
     }
 }
