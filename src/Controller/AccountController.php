7a5cf88534d2a27be3b1aaaf77eb5be174a07319
Weber-Marquiset Nolwenn <nolwenn.weber-marquiset@laposte.net>
2019-04-19 12:06:15 +0200

    account

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Security;
 
 class AccountController extends AbstractController
 {
@@ -14,6 +15,29 @@ class AccountController extends AbstractController
     {
         return $this->render('account/index.html.twig', [
             'controller_name' => 'AccountController',
+        ]);
+    }
+    /**
+     * @Route("/account/orders", name="account_orders")
+     */
+    public function AccOrders()
+    {
+        $user = $this->getUser();
+        $orders = $user->getOrders();
+        return $this->render('account/orders.html.twig', [
+            'orders' => $orders,
+        ]);
+    }
+
+    /**
+     * @Route("/account/orders/{order}", name="account_orders_show")
+     */
+    public function AccOrdersShow()
+    {
+        $user = $this->getUser();
+        $orders = $user->getOrders();
+        return $this->render('account/orders-show.html.twig', [
+            'orders' => $orders,
         ]);
     }
 }
